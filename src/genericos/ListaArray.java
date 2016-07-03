5d410ad0f95be6bb7ff4a631818756e3fc79853a
Lupercar <hotolete@gmail.com>
2016-07-03 18:22:25 +0200

    Pongo comentarios que faltaba en métodos de la clase genérica ListaArray

--- src/genericos/ListaArray.java
+++ src/genericos/ListaArray.java
@@ -6,13 +6,17 @@ import java.util.Arrays;
 public class ListaArray<T> {
 	private T[] array; 
 	
+	/**
+	 * Creo array en constructor
+	 * @param tamanho - dimension del array
+	 */
 	public ListaArray(int tamanho){
 		array = (T[])new Object[tamanho];//creamos el array 
 	}
 	
 	/**
 	 * Añade un elemento al array
-	 * @param posicion - posicion array
+	 * @param posicion - posicion del array
 	 * @param elemento - contenido array
 	 */
 	public void addArray(int posicion, T elemento){
@@ -21,7 +25,7 @@ public class ListaArray<T> {
 		else 
 			throw new RuntimeException("La posición no existe en el array."); 
 	}
-
+	
 	@Override
 	public String toString() {
 		return Arrays.toString(array); 
